0b5a999199af4120126f3f29d89b6c5df714d33a
kelvinpraises <kelvinpraises@gmail.com>
2022-03-28 04:13:30 +0100

    Updated websocket code

--- src/codes.ts
+++ src/codes.ts
@@ -1,6 +1,6 @@
 // Using custom codes as specified in https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent/code.
 const WS_CLOSE_CODES = {
-  connectionExist: 4000,
+  connectionExists: 4000,
 };
 
 export default WS_CLOSE_CODES;
